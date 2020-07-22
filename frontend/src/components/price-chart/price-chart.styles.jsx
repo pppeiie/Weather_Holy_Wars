e3d1bfcae6e1dc77034de10f1ac05fd8e5510ba8
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-07-22 20:15:07 +0700

    feat: implement price chart component

---
+++ frontend/src/components/price-chart/price-chart.styles.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const PriceChartContainer = styled.div`
+  width: 65%;
+`;
---
+++ frontend/src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HomePageContainer = styled.div`
+  margin: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
